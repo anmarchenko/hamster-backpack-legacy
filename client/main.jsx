df8e19edf48d0cf3a19d2a4fdbee71794f018fd2
altmer <igendou@gmail.com>
2016-04-11 18:30:00 +0200

    React tutorial step 2

---
+++ client/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import App from '../react/components/App.jsx';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
