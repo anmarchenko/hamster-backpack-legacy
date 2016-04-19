833534c8718b25a72da77d93cad2f133f7d5861b
altmer <igendou@gmail.com>
2016-04-19 11:08:25 +0200

    Move css files

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -8,11 +8,6 @@ Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
 
-    // check here that task belongs to project
-    if (false) {
-      throw new Meteor.Error('not-authorized');
-    }
-
     Tasks.insert({
       text,
       createdAt: new Date()
@@ -21,11 +16,21 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    // check here that task belongs to project
+    if (false) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
+
+    // check here that task belongs to project
+    if (false) {
+      throw new Meteor.Error('not-authorized');
+    }
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
