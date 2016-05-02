ee1ec4b19b047a659f79122b5f8dcaeafd3d85a4
altmer <igendou@gmail.com>
2016-05-02 18:31:54 +0200

    minor stuff

---
+++ imports/trip_templates/basic.js
@@ -0,0 +1,13 @@
+export default BasicTemplate = {
+  list1: [
+      {
+        label: 'thing1',
+        count: '2'
+      },
+      {
+        label: 'thing2',
+        count: '2*[days]'
+      },
+  ]
+
+};
