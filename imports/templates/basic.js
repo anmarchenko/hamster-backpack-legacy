529bd1a5db97bcf05d89524a8a837e1c22691d49
altmer <igendou@gmail.com>
2016-05-13 17:36:08 +0200

    template

---
+++ imports/templates/basic.js
@@ -0,0 +1,24 @@
+export default BasicTemplate = {
+  list1: {
+      name: 'list1',
+      items: [
+            {
+              label: 'thing1',
+              count: '2'
+            },
+            {
+              label: 'thing2',
+              count: '2*[days]'
+            },
+        ]
+  },
+  list2: {
+    name: 'list2',
+    items: [
+      {
+        label: 'thing3',
+        count: 5
+      }
+    ]
+  }
+};
